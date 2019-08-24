4c53e8e9ea62a763996c0c02c32f1f60ac74e3c6
Paul Grevink <paul.grevink@gmail.com>
2019-08-24 22:34:10 +0200

    Update New-SelfSignedCert.ps1

--- New-SelfSignedCert.ps1
+++ New-SelfSignedCert.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 Generate Self-signed certificate
 
 Download script from: 
@@ -8,4 +8,17 @@ https://gallery.technet.microsoft.com/scriptcenter/Self-signed-certificate-5920a
 
 #load function
 
-. "C:\VMwareDSC\New-SelfSignedCertificateEx.ps1"# generate CertNew-SelfSignedCertificateEx -Subject 'CN=Cert' -EnhancedKeyUsage 'Document Encryption' -StoreLocation LocalMachine -FriendlyName 'SelfSigned'# Check Cert$cert = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.FriendlyName -eq 'SelfSigned'}$cert # Cert can be exportedExport-Certificate -Cert $cert -FilePath C:\VMwareDSC\Cert.cer -Force
+. "C:\VMwareDSC\New-SelfSignedCertificateEx.ps1"
+
+
+# generate Cert
+New-SelfSignedCertificateEx -Subject 'CN=Cert' -EnhancedKeyUsage 'Document Encryption' -StoreLocation LocalMachine -FriendlyName 'SelfSigned'
+
+
+# Check Cert
+$cert = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.FriendlyName -eq 'SelfSigned'}
+$cert 
+
+# Cert can be exported
+Export-Certificate -Cert $cert -FilePath C:\VMwareDSC\Cert.cer -Force
+
