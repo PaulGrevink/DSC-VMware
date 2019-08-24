732aa5cb565076bf90b2b3ad8789a45005cc4ef0
Paul Grevink <paul.grevink@gmail.com>
2019-08-24 22:23:22 +0200

    Update LCM_SelfSigned.ps1

--- LCM_SelfSigned.ps1
+++ LCM_SelfSigned.ps1
@@ -14,28 +14,21 @@ Configuration LCM_SelfSigned
 
         )
 	Node $ComputerName {
-
 		Settings {
-
-			AllowModuleOverwrite = $True
-		    ConfigurationMode = 'ApplyAndMonitor'
-			RefreshMode = 'Push'
-			ConfigurationID = $guid
-            CertificateID = $thumbprint
-            }
+		AllowModuleOverwrite = $True
+		ConfigurationMode = 'ApplyAndMonitor'
+		RefreshMode = 'Push'
+		ConfigurationID = $guid
+		CertificateID = $thumbprint
+		}
 	}
 }
 
 $ComputerName = 'localhost'
-
 $cert = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.FriendlyName -eq 'SelfSigned'}
-
 $cim = New-CimSession -ComputerName $ComputerName
-
 $guid=[guid]::NewGuid()
 
 LCM_SelfSigned -ComputerName $ComputerName -Guid $guid -Thumbprint $Cert.Thumbprint -OutputPath C:\VMwareDSC\S2
-
 Set-DscLocalConfigurationManager -CimSession $cim -Path C:\VMwareDSC\S2 -Verbose
-
 Get-DscLocalConfigurationManager -CimSession $cim
